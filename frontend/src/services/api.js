90727130fc0f725dcc2be816b9fe9136b9bd9a88
vivekg <vivek1679772@gmail.com>
2026-09-06 23:00:06 +0530

    api backend conn

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useTransition } from 'react';
 
-const API_URL = `http://localhost:4000/api`;
+const API_URL = `https://shopverse-76q1.onrender.com/api`;
 
  const api = axios.create({
     baseURL:API_URL
